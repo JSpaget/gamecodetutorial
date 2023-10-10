d0ea47667139d6d7b8b5133a17512494d1b1d73c
JSpaget <james.h.moretti@gmail.com>
2023-10-10 21:00:37 +0100

    added enemies and made player move with tick speed

--- main.py
+++ main.py
@@ -1,20 +1,19 @@
 import pygame
+import random
 from pygame.locals import (
+    K_UP,
+    K_DOWN,
+    K_LEFT,
+    K_RIGHT,
     K_ESCAPE,
     KEYDOWN,
     QUIT,
 )
 
-pygame.init()
-
-screen_height=600
-screen_width=600
-
-screen=pygame.display.set_mode([screen_width,screen_height])
+screen_height = 600
+screen_width = 600
 
 
-# Define a Player object by extending pygame.sprite.Sprite
-# The surface drawn on the screen is now an attribute of 'player'
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super(Player, self).__init__()
@@ -22,26 +21,74 @@ class Player(pygame.sprite.Sprite):
         self.surf.fill((255, 255, 255))
         self.rect = self.surf.get_rect()
 
-game_running = True 
+    def update(self, pressed_keys):
+        if pressed_keys[K_UP]:
+            self.rect.move_ip(0, -5)
+        if pressed_keys[K_DOWN]:
+            self.rect.move_ip(0, 5)
+        if pressed_keys[K_LEFT]:
+            self.rect.move_ip(-5, 0)
+        if pressed_keys[K_RIGHT]:
+            self.rect.move_ip(5, 0)
+
+        if self.rect.left < 0:
+            self.rect.left = 0
+        if self.rect.right > screen_width:
+            self.rect.right = screen_width
+        if self.rect.top <= 0:
+            self.rect.top = 0
+        if self.rect.bottom >= screen_height:
+            self.rect.bottom = screen_height
+
+
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self):
+        super(Enemy, self).__init__()
+        self.surf = pygame.Surface((20, 10))
+        self.surf.fill((255, 255, 255))
+        self.rect = self.surf.get_rect(
+            center=(
+                random.randint(screen_width + 20, screen_width + 100),
+                random.randint(0, screen_height),
+            )
+        )
+        self.speed = random.randint(5, 20)
+
+    def update(self):
+        self.rect.move_ip(-self.speed, 0)
+        if self.rect.right < 0:
+            self.kill()
+
+
+pygame.init()
+
+screen = pygame.display.set_mode([screen_width, screen_height])
+player = Player()
+clock = pygame.time.Clock()
+enemies = pygame.sprite.Group()
+all_sprites = pygame.sprite.Group()
+all_sprites.add(player)
+
+
+game_running = True
 while game_running:
-  for event in pygame.event.get():
-    if event.type == KEYDOWN:
-      if event.key == K_ESCAPE:
-        game_running = False
+    for event in pygame.event.get():
+        if event.type == KEYDOWN:
+            if event.key == K_ESCAPE:
+                game_running = False
 
-    elif event.type == QUIT:
-      game_running = False
-  
+        elif event.type == QUIT:
+            game_running = False
 
-  screen.fill((255, 255, 255))
-  surf = pygame.Surface((50, 50))
-  surf.fill((0, 0, 0))
-  rect = surf.get_rect()
-  surf_center = (
-    (screen_width-surf.get_width())/2,
-    (screen_height-surf.get_height())/2
-    )
-  screen.blit(surf, surf_center)
-  pygame.display.flip()
+    pressed_keys = pygame.key.get_pressed()
+
+    player.update(pressed_keys)
+
+    screen.fill((0, 0, 0))
+    for entity in all_sprites:
+        screen.blit(entity.surf, entity.rect)
+
+    pygame.display.flip()
+    clock.tick(30)
 
 pygame.quit()
